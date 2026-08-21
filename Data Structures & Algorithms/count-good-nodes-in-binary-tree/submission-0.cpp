a2cfcbfb73ed9f48200b8c40ceca044d7378c491
Sneha Adala <120920554+adalasneha@users.noreply.github.com>
2026-08-21 23:36:38 +0530

    Add: count-good-nodes-in-binary-tree - submission-0

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        return dfs(root, root->val);
+    }
+
+private:
+    int dfs(TreeNode* node, int maxVal) {
+        if (!node) {
+            return 0;
+        }
+
+        int res = (node->val >= maxVal) ? 1 : 0;
+        maxVal = max(maxVal, node->val);
+        res += dfs(node->left, maxVal);
+        res += dfs(node->right, maxVal);
+        return res;
+    }
+};
